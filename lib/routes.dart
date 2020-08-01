3e56f364411e9e12f4d3ca4634179becbe624f8a
shi-lai-mu <478889187@qq.com>
2020-08-01 18:44:34 +0800

    feat: 路由返回值

--- lib/routes.dart
+++ lib/routes.dart
@@ -37,7 +37,60 @@ class NewRoutes extends StatelessWidget {
         title: Text('New route'),
       ),
       body: Center(
-        child: Text('This is new route'),
+        child: Column(
+          children: <Widget>[
+            RaisedButton(
+              child: Text('NAVIGATOR BACK'),
+              textColor: Colors.red,
+              onPressed: () async {
+                var result = await Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return TipRoute(
+                        text: '我是提示xxxx',
+                      );
+                    }
+                  )
+                );
+                print("路由返回值: $result");
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class TipRoute extends StatelessWidget {
+  TipRoute({
+    Key key,
+    @required this.text,
+  }) : super(key: key);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TipRoute'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(18.0),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Text(text),
+              RaisedButton(
+                onPressed: () => Navigator.pop(context, '我是返回值'),
+                child: Text('返回'),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
